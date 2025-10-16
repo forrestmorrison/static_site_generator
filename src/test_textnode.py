e87ed822a846e23d55c07567660c9cc3739de465
forrestmorrison <forrestmorrison@gmail.com>
2025-10-16 10:35:06 -0500

    added unit tests

--- src/test_textnode.py
+++ src/test_textnode.py
@@ -9,6 +9,18 @@ class TestTextNode(unittest.TestCase):
         node2 = TextNode("This is a text node", TextType.BOLD)
         self.assertEqual(node, node2)
 
+    def test_not_eq(self):
+        node = TextNode("This is a text node", TextType.ITALIC)
+        node2 = TextNode("This is also a text node", TextType.TEXT)
+        self.assertNotEqual(node, node2)
+
+    def test_no_url(self):
+        node = TextNode("This text node has no URL", TextType.TEXT)
+        return node.url is None
+    
+    def test_is_url(self):
+        node = TextNode("This text node has a URL", TextType.TEXT, "https://wwww.textnode.com")
+        return node.url is not None
 
 if __name__ == "__main__":
     unittest.main()
